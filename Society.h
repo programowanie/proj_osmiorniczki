49ff4d3d19db7883ba0c46429d86cef82017f953
pawel-zolty <226115@student.pwr.edu.pl>
2016-06-02 23:49:38 +0200

    DO OCENY

--- Society.h
+++ Society.h
@@ -9,6 +9,8 @@ public:
 	Society();
 
 	int social_mood(){return _social_mood;}
+	int right(){return _right;}
+	int left(){return _left;}
 
 	void symulate(int,int);
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <ctime>
+#include <unistd.h>
 
 #include "Parliament.h"
 #include "Society.h"
+
+#define INTERVAL 1000000
 
 int main()
 {
@@ -22,6 +26,7 @@ int main()
 		<<"\nSa z nami poslowie Porozumienia Oblakanych:\n"<<std::endl;
 	for(int i=0;i<Sejm.porozumienie_oblakanych().number_of_MPs();i++)
 	{
+		usleep(INTERVAL);
 		std::cout<<Sejm.porozumienie_oblakanych().vec_of_MPs()[i].description()
 			<<std::endl;
 	}
@@ -29,6 +34,7 @@ int main()
 	std::cout<<"\n\n\noraz poslowie Patriotow i Spiskow:\n"<<std::endl;
 	for(int i=0;i<Sejm.patrioci_i_spiski().number_of_MPs();i++)
 	{
+		usleep(INTERVAL);
 		std::cout<<Sejm.patrioci_i_spiski().vec_of_MPs()[i].description() 
 			<<std::endl;
 	}
@@ -36,12 +42,29 @@ int main()
 //Symulacja
 	for(int i=0;i<60;i++)
 	{
+		usleep(INTERVAL);
 		int * decision=ruling_party.decision();
 		Polska.symulate(decision[0],decision[1]);
 		if(Polska.social_mood()<=0)
 		{
 			Sejm.set_fire();
 			break;
+		}
+		else
+			if(Polska.social_mood()>=100)
+			{
+				std::cout<<"\n\n\n\t\t\t\t\t\t\tKaczor KRUL!!!";
+				break;		
+			}
+		if(Polska.right()>=100)
+		{
+			std::cout<<"\n\n\n\t\t\t\t\t\t\tIndoktrynacja I KATO FASZYZM!!!";
+			break;
+		}
+		if(Polska.left()>=100)
+		{
+			std::cout<<"\n\n\n\t\t\t\t\t\t\tIndoktrynacja LGBT IN VITRO i przelsadowanie chrzescijan!!!";
+			break;	
 		}
 	}
 	std::cout<<"\n\n\n";
